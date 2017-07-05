a1c5fc006d6342db0c1f565fc3da091a65eb91e2
emmanuelb <emmanuelb@ARTSOFT21.ARTSOFT.RO>
2017-07-05 16:49:57 +0300

    MVC Project + Tasks

---
+++ app/models/project.rb
@@ -0,0 +1,8 @@
+class Project < ApplicationRecord
+
+	has_many :tasks, dependent: :destroy
+
+	validates :title, presence: true,
+                    length: { minimum: 5 }
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get 'welcome/index'
+
+	resources :projects
+
+	resources :projects do
+		resources :tasks
+	end
+
+	root 'welcome#index'
+
 end
