9c115e88d5c998dc881b67e5ac7c6f36756d28b3
Sergiu Nichitean <sergiu.nichitean@gmail.com>
2017-07-06 11:32:36 +0300

    fixed routes, added stuff to gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   resources :users
 
   root :to => "sessions#login"
-  
-  match ':controller(/:action(/:id))(.:format)', :to => 'controller#action', :via => [:get, :post]
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	get 'welcome/index'
 
@@ -13,5 +9,9 @@ Rails.application.routes.draw do
 
 	resources :projects do
 		resources :tasks
-	end
+  end
+  
+	match ':controller(/:action(/:id))(.:format)', :to => 'controller#action', :via => [:get, :post]
+
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
