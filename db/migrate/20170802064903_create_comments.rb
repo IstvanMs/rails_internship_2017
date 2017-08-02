3f30bd00a24f0db4be1ff39512b9fd2946abe437
istvanm <istvanm@ARTSOFT112.artsoft.ro>
2017-08-02 10:57:38 +0300

    08/02 09:58

--- db/migrate/20170802064903_create_comments.rb
+++ db/migrate/20170802064903_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.references :commenter, foreign_key: true
+      t.references :user, foreign_key: true
       t.text :text
-      t.references :reply_to, foreign_key: true
+      t.integer :reply_to
 
       t.timestamps
     end
