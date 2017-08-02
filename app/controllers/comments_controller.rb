f0b320f38368387509d328fa311f03c39a78f238
istvanm <istvanm@ARTSOFT112.artsoft.ro>
2017-08-02 16:58:42 +0300

    08/02 16:58

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+	before_action :authenticate_user, :only => [:create]
+
+	def reply
+	end
+
+	def create
+		@task = Task.find(params[:task_id])
+		@comment = @task.comments.create({ :user => User.find(comment_params['user']), :text => comment_params['text'], :reply_to => comment_params['reply_to']})
+		redirect_to task_path(@task)
+	end
+
+	def destroy
+		@task = Task.find(params[:task_id])
+		@comment = @task.comments.find(params[:id])
+		@comment.destroy	
+		redirect_to task_path(@task)
+	end
+
+	private
+		def comment_params
+			params.require(:comment).permit(:user, :text, :reply_to)
+		end
+end
