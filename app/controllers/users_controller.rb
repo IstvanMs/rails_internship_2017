07266a5594b344ea524659881b2de891c50a05f0
istvanm <istvanm@ARTSOFT112.artsoft.ro>
2017-08-10 15:45:14 +0300

    08/10 15:42

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,15 @@ class UsersController < ApplicationController
   before_action :authenticate_user, :only => [:show, :profile]
   
   def index
+    @admin = User.find(session[:user_id])
+    @company = Company.find(@admin.company_id)
+    @roles = Role.where(:company_id => @company.id)
     @search_par = params[:search]
 
     if @search_par == nil || @search_par == ''
-  	  @users = User.all.order(:role,:username)
+      @users = User.where(:type => nil,:company_id => @company.id).order(:role,:username)
     else
-      @users = User.where('username like ?', '%' + @search_par + '%').order(:role,:username)
+      @users = User.where('type = ? and username like ? and company_id = ?',nil, '%' + @search_par + '%', @company.id).order(:role,:username)
     end
     @user = User.new
   end
@@ -68,7 +71,7 @@ class UsersController < ApplicationController
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def reset
@@ -77,57 +80,68 @@ class UsersController < ApplicationController
 
   def show
     if @current_user.id == params[:id] || @current_user.role == 'Admin'
-  	 @user = User.find(params[:id])
+     @user = User.find(params[:id])
+     @role = Role.find(@user.role_id)
     else
       redirect_to root_path
     end
   end
 
   def edit
-  	@user = User.find(params[:id])
-    @users = User.all.order(:role,:username)
+    @admin = User.find(session[:user_id])
+    @company = Company.find(@admin.company_id)
+    @roles = Role.where(:company_id => @company.id)
+    @user = User.find(params[:id])
+    @users = User.where(:type => nil).order(:role,:username)
     render 'index'
   end
 
   def update 
-  	@user = User.find(params[:id])
+    @admin = User.find(session[:user_id])
+    @company = Company.find(@admin.company_id)
+    @roles = Role.where(:company_id => @company.id)
+    @user = User.find(params[:id])
+    @users = User.where(:type => nil,:company_id => @company.id).order(:role,:username)
+    par = user_params
+    role_dash = Role.find(par[:role_id]).dashboard.capitalize
 
-  	if @user.update(user_params)
-  	  redirect_to users_path
-  	else
-  		render 'edit'
-  	end
+    if @user.update({:username => par[:username], :password => par[:password], :password_confirmation => par[:password_confirmation], :email => par[:email], :role => role_dash, :company_id => par[:company_id], :role_id => par[:role_id]})
+      redirect_to users_path
+    else
+      render 'index'
+    end
   end
 
   def destroy
-  	@user = User.find(params[:id])
-  	@user.destroy
+    @user = User.find(params[:id])
+    @user.destroy
 
-  	redirect_to users_path
+    redirect_to users_path
   end
 
   def create
-  	@user = User.new(user_params)
-    @users = User.all.order(:role,:username)
+    @admin = User.find(session[:user_id])
+    @company = Company.find(@admin.company_id)
+    @roles = Role.where(:company_id => @company.id)
+    par = user_params
+    role_dash = Role.find(par[:role_id]).dashboard.capitalize
+    @user = User.new({:username => par[:username], :password => par[:password], :password_confirmation => par[:password_confirmation], :email => par[:email], :role => role_dash, :company_id => par[:company_id], :role_id => par[:role_id]})
+    @users = User.where(:type => nil,:company_id => @company.id).order(:role,:username)
 
-  	if @user.save 
+    if @user.save 
       MailerMailer.new_user(@user).deliver
-  		flash[:notice] = "Saved!"
-  		flash[:color] = "valid"
-  		redirect_to users_path
-  	else
-  		flash[:notice] = "Invalid form!"
-  		flash[:color] = "invalid"
-  		render "index"
-  	end
-  end
-
-  def get_admins(company_id)
-    @admins = U
+      flash[:notice] = "Saved!"
+      flash[:color] = "valid"
+      redirect_to users_path
+    else
+      flash[:notice] = "Invalid form!"
+      flash[:color] = "invalid"
+      render "index"
+    end
   end
 
   private 
-  	def user_params
-  		params.require(:user).permit(:username, :password, :password_confirmation, :email, :role)
-  	end
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation, :email, :company_id, :role_id)
+    end
 end
