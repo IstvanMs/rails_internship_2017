e69db94acbf34aaa9696669d4e8cac853e348eca
istvanm <istvanm@ARTSOFT112.artsoft.ro>
2017-08-10 15:51:45 +0300

    08/10 15:42

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,7 +5,7 @@ class NotesController < ApplicationController
 	def index
     case @current_user.role
     when 'Manager'
-      @notes = Note.all
+      @notes = Note.where
     when 'Client'
       @table = Project.joins(:tasks , :projectUsers => :user ).where(:project_users => { :user_id => @current_user}).uniq
       @notes = Note.where(:visibility => 'general')
