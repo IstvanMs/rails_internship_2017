ba35dc8997d9d71ec296179b63d51fe355386550
istvanm <istvanm@ARTSOFT112.artsoft.ro>
2017-08-10 16:04:29 +0300

    08/10 16:04

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,20 +3,21 @@ class NotesController < ApplicationController
   before_action :authenticate_user, :only => [:show, :index]
 	
 	def index
+    @company = Company.find(@current_user.company_id)
     case @current_user.role
     when 'Manager'
-      @notes = Note.all
+      @notes = Note.where(:company_id => @company.id)
     when 'Client'
       @table = Project.joins(:tasks , :projectUsers => :user ).where(:project_users => { :user_id => @current_user}).uniq
-      @notes = Note.where(:visibility => 'general')
-      @notes += Note.where(:visibility => @table.collect{|p| 'project-'+p.id.to_s})
-      @notes += Note.where(:visibility => @table.collect(&:task_ids).uniq.flatten.collect{|id| 'task-' + id.to_s})
-      @notes += Note.where(:visibility => 'user-'+ @current_user.id.to_s)
+      @notes = Note.where(:visibility => 'general', :company_id => @company.id)
+      @notes += Note.where(:visibility => @table.collect{|p| 'project-'+p.id.to_s}, :company_id => @company.id)
+      @notes += Note.where(:visibility => @table.collect(&:task_ids).uniq.flatten.collect{|id| 'task-' + id.to_s}, :company_id => @company.id)
+      @notes += Note.where(:visibility => 'user-'+ @current_user.id.to_s, :company_id => @company.id)
     else
-      @notes = Note.where(:visibility => 'general')
-      @notes += Note.where(:visibility => ProjectUser.where(:user_id => @current_user.id).collect{|p| 'project-' + p.project_id.to_s})
-      @notes += Note.where(:visibility => Task.where(:assigned_user => @current_user).collect{|t| 'task-' + t.id.to_s})
-      @notes += Note.where(:visibility => 'user-'+ @current_user.id.to_s)
+      @notes = Note.where(:visibility => 'general', :company_id => @company.id)
+      @notes += Note.where(:visibility => ProjectUser.where(:user_id => @current_user.id).collect{|p| 'project-' + p.project_id.to_s}, :company_id => @company.id)
+      @notes += Note.where(:visibility => Task.where(:assigned_user => @current_user).collect{|t| 'task-' + t.id.to_s}, :company_id => @company.id)
+      @notes += Note.where(:visibility => 'user-'+ @current_user.id.to_s, :company_id => @company.id)
     end
     @notes = Note.where(:id => @notes.collect(&:id)).order(:visibility)
     @lengths = {'notes' => @notes.length}
@@ -24,6 +25,7 @@ class NotesController < ApplicationController
   end
 
 	def show
+    @company = Company.find(@current_user.company_id)
   	@note = Note.find(params[:id])
     proj = ProjectUser.find_by(:project_id  => @note.visibility.split('-')[1], :user_id => @current_user.id)
     task = Task.find_by(:id => @note.visibility.split('-')[1], :assigned_user => @current_user)
@@ -35,23 +37,26 @@ class NotesController < ApplicationController
 	end
 
 	def new
-    @users = User.where.not(:id => @current_user.id)
-    @projects = Project.all
-    @tasks = Task.all
+    @company = Company.find(@current_user.company_id)
+    @users = User.where('id != ? and company_id = ?', @current_user.id, @company.id)
+    @projects = Project.where(:company_id => @company.id)
+    @tasks = Task.where(:company_id => @company.id)
 		@note = Note.new
 	end
 
 	def edit
-    @users = User.where.not(:id => @current_user.id)
-    @projects = Project.all
-    @tasks = Task.all
+    @company = Company.find(@current_user.company_id)
+    @users = User.where('id != ? and company_id = ?', @current_user.id, @company.id)
+    @projects = Project.where(:company_id => @company.id)
+    @tasks = Task.where(:company_id => @company.id)
 		@note = Note.find(params[:id])
 	end
 
 	def create
-    @users = User.where.not(:id => @current_user.id)
-    @projects = Project.all
-    @tasks = Task.all
+    @company = Company.find(@current_user.company_id)
+    @users = User.where('id != ? and company_id = ?', @current_user.id, @company.id)
+    @projects = Project.where(:company_id => @company.id)
+    @tasks = Task.where(:company_id => @company.id)
     @note = Note.new
     @note.text = params[:note][:text]
     case(params[:note_type])
@@ -65,6 +70,7 @@ class NotesController < ApplicationController
         @note.visibility = 'user-'+params[:selected_id]
       else puts 'Type error!'
     end
+    @note.company_id = params[:note][:company_id]
 		if @note.save
   		redirect_to notes_path
 		else
@@ -91,7 +97,7 @@ class NotesController < ApplicationController
 
 	private
   	def note_params
-    	params.require(:note).permit(:text, :visibility)
+    	params.require(:note).permit(:text, :visibility, :company_id)
   	end
 
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -5,28 +5,29 @@ class Note < ApplicationRecord
 	            length: { minimum: 6 }
 
 	def self.get_notes(user)
+		@company = Company.find(user.company_id)
 		case user.role
 			when 'Employee'
-				@notes = Note.where(:visibility => 'general')
-				@notes += Note.where(:visibility => ProjectUser.where(:user_id => user).collect{|p| 'project-' + p.project_id.to_s})
-				@notes += Note.where(:visibility => Task.where(:assigned_user => user).collect{|t| 'task-' + t.id.to_s})
-				@notes += Note.where(:visibility => 'user-'+ user.id.to_s)
+				@notes = Note.where(:visibility => 'general', :company_id => @company.id)
+				@notes += Note.where(:visibility => ProjectUser.where(:user_id => user).collect{|p| 'project-' + p.project_id.to_s}, :company_id => @company.id)
+				@notes += Note.where(:visibility => Task.where(:assigned_user => user).collect{|t| 'task-' + t.id.to_s}, :company_id => @company.id)
+				@notes += Note.where(:visibility => 'user-'+ user.id.to_s, :company_id => @company.id)
 		        @notes = @notes.sort_by{|n| n.visibility}
 			when 'Admin'
-				@notes = Note.where(:visibility => 'general')
-				@notes += Note.where(:visibility => ProjectUser.where(:user_id => user).collect{|p| 'project-' + p.project_id.to_s})
-				@notes += Note.where(:visibility => Task.where(:assigned_user => user).collect{|t| 'task-' + t.id.to_s})
-				@notes += Note.where(:visibility => 'user-'+ user.id.to_s)
+				@notes = Note.where(:visibility => 'general', :company_id => @company.id)
+				@notes += Note.where(:visibility => ProjectUser.where(:user_id => user).collect{|p| 'project-' + p.project_id.to_s}, :company_id => @company.id)
+				@notes += Note.where(:visibility => Task.where(:assigned_user => user).collect{|t| 'task-' + t.id.to_s}, :company_id => @company.id)
+				@notes += Note.where(:visibility => 'user-'+ user.id.to_s, :company_id => @company.id)
 		        @notes = @notes.sort_by{|n| n.visibility}
 			when 'Client'
 				@table = Project.joins(:tasks , :projectUsers => :user ).where(:project_users => { :user_id => user})
-				@notes = Note.where(:visibility => 'general')
-				@notes += Note.where(:visibility => ProjectUser.where(:user_id => user).collect{|p| 'project-' + p.project_id.to_s})
+				@notes = Note.where(:visibility => 'general', :company_id => @company.id)
+				@notes += Note.where(:visibility => ProjectUser.where(:user_id => user).collect{|p| 'project-' + p.project_id.to_s}, :company_id => @company.id)
 				@notes += Note.where(:visibility => @table.collect(&:task_ids).uniq.flatten.collect{|id| 'task-' + id.to_s})
-				@notes += Note.where(:visibility => 'user-'+ user.id.to_s)
+				@notes += Note.where(:visibility => 'user-'+ user.id.to_s, :company_id => @company.id)
 		        @notes = @notes.sort_by{|n| n.visibility}
 			when 'Manager'
-				@notes = Note.all
+				@notes = Note.where(:company_id => @company.id)
 				@notes = @notes.sort_by{|n| n.visibility}
 			else
 				puts 'Role error!'
