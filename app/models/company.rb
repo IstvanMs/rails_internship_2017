e8be29f1d41ffeb1b5f40a55ecb272c4e3b02ac4
emmanuelb <emmanuelb@ARTSOFT21.ARTSOFT.RO>
2017-08-11 09:26:52 +0300

    first

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,8 +4,8 @@ class Company < ApplicationRecord
   has_many :projects
   has_many :notes
 
-  def self.get_admins
-    User.where :role => "Admin"
+  def self.get_admins(company_id)
+    User.where( :role => "Admin", :company_id => company_id )
   end
 
 end
